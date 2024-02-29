f469154d2bc6b391b584c1bbaf2eb2199937d223
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2024-02-29 22:04:30 +0100

    update code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-// Say hello let'us build a calculator
-alert("Hello there, let'us build a calculator");
+const display = document.getElementById('display');
+
+const appendToDisplay = (input) => {
+    display.value += input;
+};
+
+const calculate = () => {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+};
+
+const clearDisplay = () => {
+    display.value = '';
+};
